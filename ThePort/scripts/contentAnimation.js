76e712866aeaedc77cbb5a40f309491873a36222
TDonnally <59318416+TDonnally@users.noreply.github.com>
2020-02-09 20:45:40 -0500

    Added responsive js

--- ThePort/scripts/contentAnimation.js
+++ ThePort/scripts/contentAnimation.js
@@ -3,9 +3,15 @@ function openContent(a){
     content.parentElement.style.width = "100%";
     content.parentElement.style.margin = "0%";
     
-    content.style.width = "50%";
-    content.style.marginLeft = "25%";
-    content.style.marginRight = "25%";
+    if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)){
+        content.style.width = "100%";
+        content.style.margin = "0%";
+    }
+    else{
+        content.style.width = "50%";
+        content.style.marginLeft = "25%";
+        content.style.marginRight = "25%";
+    }
     
     /*setTimeout(() => {content.parentElement.style.borderRadius = "0%";
                     content.style.borderRadius = "0%";  }, 100);*/
@@ -21,4 +27,7 @@ function closeContent(a){
     content.style.marginLeft = "50%";
     content.style.marginRight = "50%";
 }
+function isMobileDevice() {
+    return (typeof window.orientation !== "undefined") || (navigator.userAgent.indexOf('IEMobile') !== -1);
+};
 
